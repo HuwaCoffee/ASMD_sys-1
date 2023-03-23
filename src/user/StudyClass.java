a5372e5047e618789ac1592de64743c8bb27a99c
HuwaCoffee <116842137+HuwaCoffee@users.noreply.github.com>
2023-03-23 09:26:55 +0800

    Update and rename Class.java to StudyClass.java
    更改Class为StudyClass

--- src/user/StudyClass.java
+++ src/user/StudyClass.java
@@ -3,23 +3,27 @@ package user;
 import java.util.ArrayList;
 /*班级类中包含某个班级的所有学生信息
 可以通过声明一个班级类型的变量进行学校信息的存储和增删改查*/
-public class Class {
+public class StudyClass {
     private ArrayList<Student>studentList;   //储存学生信息的数组
     private int ClassId;                     //班级id
+    private String ClassName;                //班级名称
 
     public ArrayList<Student> getStudentList() {
         return studentList;
     }
-
     public void setStudentList(ArrayList<Student> studentList) {
         this.studentList = studentList;
     }
-
     public int getClassId() {
         return ClassId;
     }
-
     public void setClassId(int classId) {
         ClassId = classId;
+    }
+    public String getClassName() {
+        return ClassName;
+    }
+    public void setClassName(String className) {
+        ClassName = className;
     }
 }
