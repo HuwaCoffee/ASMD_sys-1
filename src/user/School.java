f4199976b1f547ced9a0ca47aee9b5e7f73343fd
kk0354 <124652250+kk0354@users.noreply.github.com>
2023-03-23 13:03:16 +0800

    Update School.java

--- src/user/School.java
+++ src/user/School.java
@@ -4,17 +4,10 @@ import java.util.ArrayList;
 /*学校类中包含某个学校的所有班级信息
 可以通过声明一个学校类型的变量进行学校信息的存储和增删改查*/
 public class School {
-    private ArrayList<Class>classList;   //储存班级信息的数组
     private int SchoolId;                //学校id
+    private String SchoolName;           //学校name
 
     //getter和setter
-    public ArrayList<Class> getClassList() {
-        return classList;
-    }
-
-    public void setClassList(ArrayList<Class> classList) {
-        this.classList = classList;
-    }
 
     public int getSchoolId() {
         return SchoolId;
@@ -24,5 +17,12 @@ public class School {
         SchoolId = schoolId;
     }
 
+    public String getSchoolName() {
+        return SchoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        SchoolName = schoolName;
+    }
 
 }
