87deaeb2dca02c03ecb8403b1a309fc72bd90937
HuwaCoffee <116842137+HuwaCoffee@users.noreply.github.com>
2023-03-24 11:13:47 +0800

    Update CheckingAction.java

--- src/Action/CheckingAction.java
+++ src/Action/CheckingAction.java
@@ -13,20 +13,26 @@ public class CheckingAction implements Action{
     private ArrayList<Boolean> checkPaper=new ArrayList<>();//储存一个判断对错的数组，判断用户输入是否正确，如果正确，对应题号为true，错误则为false
     private ArrayList<BinaryOperation>problems;  //储存试卷信息，信息由前端发送
     private ArrayList<Integer>answ;              //储存答案信息，信息由前端发送
-    //数据库相关属性
-    private MysqlTool mysql=new MysqlTool();
-    private Connection con;
-    private Statement statement;
-    
-    
+
     public ArrayList<Boolean> getCheckPaper() {
         return checkPaper;
     }
+    public ArrayList<BinaryOperation> getProblems() {
+        return problems;
+    }
+    public void setProblems(ArrayList<BinaryOperation> problems) {
+        this.problems = problems;
+    }
+    public ArrayList<Integer> getAnsw() {
+        return answ;
+    }
+    public void setAnsw(ArrayList<Integer> answ) {
+        this.answ = answ;
+    }
 
-    
     @Override
     public String execute() throws Exception {
-        con=mysql.openDB();
+
         //判断学生输入的信息是否正确，提取前端发送来的题目数组和对应结果，后端函数进行判断，返回boolen类型的数组，如果正确返回1.如果错误返回0
             for(int i=0;i<problems.size();i++){
                 checkPaper.add(true);      //将checkPaper赋初值
