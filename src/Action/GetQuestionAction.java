0b324fec10f4c266cd16c2f163267100f5a7551f
HuwaCoffee <116842137+HuwaCoffee@users.noreply.github.com>
2023-03-24 11:20:21 +0800

    Update GetQuestionAction.java

--- src/Action/GetQuestionAction.java
+++ src/Action/GetQuestionAction.java
@@ -1,6 +1,6 @@
 package Action;
 
-import DataBaseGet.DataBaseGet;
+import DataBase.DataBaseGet;
 import com.opensymphony.xwork2.ActionContext;
 import exercise.BinaryOperation;
 import java.util.ArrayList;
